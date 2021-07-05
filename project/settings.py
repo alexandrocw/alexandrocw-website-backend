c3dd20e406750c4b709ac17597ce442e7a75c1fb
lKingDGl <alexandrowijaya@yahoo.co.id>
2021-07-05 08:58:05 +0700

    Configure dotenv

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from boto.s3.connection import S3Connection
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = S3Connection(os.environ["SECRET_KEY"])
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
